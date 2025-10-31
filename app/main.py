65eda35c5f8a18a67342ae0fca1c332715ab750c
Taubay Kozh <taubay@example.com>
2025-10-31 19:05:52 +0500

    export resume to file

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI, Query, HTTPException, status
 from typing import Literal
 from app.services import resume_generator
-from app.models import ResumeRequest
+from app.models import ResumeRequest, ResumeResponse
+from app.utils.expoter import generate_resume_docx
 
 app = FastAPI()
 
 
 @app.post("/generate_resume", summary="Получение резюме")
-def create_resume(data: ResumeRequest, lang: Literal["en", "ru", "kz"] = Query("en")):
+def create_resume(
+    data: ResumeRequest,
+    lang: Literal["en", "ru", "kz"] = Query("en"),
+    gen_file: bool = False,
+):
     result = resume_generator.request(input_data=data, lang=lang)
     if not result.get("ok"):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=result.get("error"),
         )
-    return result.get("result")
+    resume = result.get("result")
+    if gen_file:
+        generate_resume_docx(resume.dict(), title_file=data.full_name)
+    return resume
+
+
+@app.post("/export_resume", summary="Генерация файла резюме")
+def generate_resume_file(data: ResumeResponse):
+    path = generate_resume_docx(data.dict())
+    return path
---
+++ app/utils/expoter.py
@@ -0,0 +1,33 @@
+from docx import Document
+from pathlib import Path
+
+
+def generate_resume_docx(
+    data: dict, lang: str = "en", title_file: str | None = None
+) -> Path:
+    doc = Document()
+
+    doc.add_heading(data.get("summary", "Resume"), level=1)
+    doc.add_paragraph(" ")
+
+    for section in data["resume_text"].split("## "):
+        section = section.strip()
+        if not section:
+            continue
+        lines = section.split("\n")
+        title = lines[0].strip()
+        doc.add_heading(title, level=2)
+        for line in lines[1:]:
+            if line.startswith("- "):
+                p = doc.add_paragraph(line[2:], style="List Bullet")
+            else:
+                p = doc.add_paragraph(line)
+
+    name = (
+        f"resume_agent_demo_{title_file.replace(' ', '_')}.docx"
+        if title_file
+        else "resume_agent_demo.docx"
+    )
+    path = Path(name)
+    doc.save(path)
+    return path
